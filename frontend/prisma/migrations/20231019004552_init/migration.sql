021fe9d7b633c0850f4ad9aeed7ba6a59a3f3276
edhoi <139027322+edhoi@users.noreply.github.com>
2023-10-22 02:32:14 -0700

    RESTART

--- frontend/prisma/migrations/20231019004552_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Admin" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "displayOption" TEXT NOT NULL,
-    "message" TEXT,
-    "website" TEXT
-);
--- frontend/src/app/lib/admin.js
+++
@@ -1,5 +0,0 @@
-export function getAdmin() {
-    return prisma.admin.findUnique({where: {
-        id: '7a548596-d0ac-4897-9d42-1e29dbb049a4'
-    }})
-  }
